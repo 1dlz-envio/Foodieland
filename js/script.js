fa53e6137e703e8d2f50a4aa63b77f8b33044bd8
1dlz.envio <1dlz.envio@gmail.com>
2022-07-24 00:36:28 +0300

    add block Category. Add JS for button view or hide cards

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-
+//init splide slider
     var splide = new Splide( '.splide',{
     type   : 'loop',
   	 padding: '5rem',
@@ -17,5 +17,24 @@ const burger = document.querySelector('#burger');
 if (burger) {
 	burger.addEventListener("click", function(e) {
 		burgerMenu.classList.toggle('open');
+	});
+};
+
+//show or hide block Category
+
+const	categoryButton = document.querySelector('#foodCategory__toggleCaterory');
+const categoryWrapper = document.querySelector('#foodCategory__wrapper');
+let countCardsCategory = document.querySelector('#foodCategory__foodCards');
+
+let wConteiner = categoryWrapper.clientWidth;
+let wCountCardsCategory =  countCardsCategory.children.length * 180;
+
+
+if ((wCountCardsCategory / wConteiner) <= 0.625) {
+	categoryButton.classList.toggle('buttonCategoryOff');
+	countCardsCategory.classList.toggle('spaceAround');
+} else {
+	categoryButton.addEventListener("click", function(e) {
+		categoryWrapper.classList.toggle('category__action');
 	});
 };
